a65827e67daaedd2928bca96bd77d4800999e642
Phil Hawksworth <phil@netlify.com>
2019-06-10 11:54:05 +0100

    tiny tidy of config

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,13 @@
-const UglifyJS              = require("uglify-js");
-const syntaxHighlightPlugin = require("@11ty/eleventy-plugin-syntaxhighlight");
-
-
 module.exports = function(eleventyConfig) {
-
-  // syntax highlighting plugin to use in any md files
-  eleventyConfig.addPlugin(syntaxHighlightPlugin, {
-    templateFormats: "md"
-  });
 
   // static passthroughs
   eleventyConfig.addPassthroughCopy("src/site/assets");
   eleventyConfig.addPassthroughCopy("src/site/favicon.ico");
-  eleventyConfig.addPassthroughCopy("src/site/favicon.ico");
-
 
   // compress and combine js files
   eleventyConfig.addFilter("jsmin", function(code) {
-      let minified = UglifyJS.minify(code);
+    const UglifyJS = require("uglify-js");
+    let minified = UglifyJS.minify(code);
       if( minified.error ) {
           console.log("UglifyJS error: ", minified.error);
           return code;
@@ -25,10 +15,9 @@ module.exports = function(eleventyConfig) {
       return minified.code;
   });
 
-
   // minify the html output for bonus points from the web perf pixies
-  const htmlmin = require("html-minifier");
   eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    const htmlmin = require("html-minifier");
     if( outputPath.endsWith(".html") ) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
@@ -40,13 +29,13 @@ module.exports = function(eleventyConfig) {
     return content;
   });
 
-  // Set up main folders and temaplet options
+  // Set up main folders and template options
   return {
     dir: {
       input: "src/site",
       output: "dist",
     },
-    templateFormats : ["njk", "md", "11ty.js", "css"],
+    templateFormats : ["njk", "md", "11ty.js"],
     htmlTemplateEngine : "njk",
     markdownTemplateEngine : "njk",
     passthroughFileCopy: true
