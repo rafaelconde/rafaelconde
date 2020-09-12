49f4ba97ee242b3905b00d46b44ccddd46c74068
Rafael Conde <rafahari@me.com>
2020-09-12 17:29:57 +0000

    Refactoring the rest, plus Thwip section

--- src/site/_includes/js/scripts.js
+++ src/site/_includes/js/scripts.js
@@ -17,3 +17,77 @@ function meetingSomeone() {
 }
 
 meetingSomeone();
+
+
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  }
+})
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  m = checkTime(m);
+  h = h % 12;
+  document.getElementById('clock').innerHTML =
+  h + ":" + m;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+startTime()
+
+function playSound(id) {
+  var sound = document.getElementById(id);
+  sound.play();
+}
+
+function playSound_Webshooter() {
+  var soundFiles = document.getElementsByClassName('webshooter');
+  const index = getRandomInt(soundFiles.length)
+  
+  soundFiles[index].play();
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function presentHelp() {
+  document.getElementById("WKMainView").style.transform = "translateX(-184px)";
+}
+
+function presentAbout() {
+  document.getElementById("WKMainView").style.transform = "translateX(-184px)";
+  document.getElementById("WKHelpView").style.zIndex = "2";
+  document.getElementById("WKAboutView").style.zIndex = "3";
+}
+
+function backToMain() {
+  document.getElementById("WKMainView").style.transform = "translateX(0px)";
+  document.getElementById("WKHelpView").style.zIndex = "3";
+  document.getElementById("WKAboutView").style.zIndex = "2";
+}
+
+var video = document.getElementById('demo-video');
+
+function toggleMute(){
+  video.muted = !video.muted;
+  
+  if (video.muted) {
+    document.getElementById("mute-switch-button").classList.remove("mute-switch-on");
+    document.getElementById("mute-switch-button").classList.add("mute-switch-off");
+  } else {
+    document.getElementById("mute-switch-button").classList.remove("mute-switch-off");
+    document.getElementById("mute-switch-button").classList.add("mute-switch-on");
+  }
+}
