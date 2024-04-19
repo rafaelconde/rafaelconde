6cc8c1d2bc9549229a64489a9f12cda2a7103aec
Rafael Conde <rafa@rafa.design>
2024-04-19 13:34:56 +0200

    Add Spatialty Coffee section

--- src/site/_includes/js/scripts.js
+++ src/site/_includes/js/scripts.js
@@ -198,3 +198,24 @@ video.addEventListener('mouseover', function() {
 video.addEventListener('mouseout', function() {
 	video.muted = true;
 });
+
+// Spatialty Coffee Parallax Effect
+const degrees = 8;
+const element = document.querySelector('.appIcon');
+
+const onPointerMove = (pointer) => {
+  const icon = element.getBoundingClientRect();
+  const halfSize = icon.width / 2;
+
+  const xDist = minMaxValue(-(icon.x - pointer.x + halfSize), -halfSize, halfSize);
+  const yDist = minMaxValue(icon.y - pointer.y + halfSize, -halfSize, halfSize);
+
+  const x = Math.round(xDist / (halfSize / 100)) / 100;
+  const y = Math.round(yDist / (halfSize / 100)) / 100;
+
+  element.style.transform = `rotateX(${y * degrees}deg) rotateY(${x * degrees}deg)`;
+};
+
+const minMaxValue = (value, min, max) => Math.min(Math.max(value, min), max);
+
+window.addEventListener('pointermove', onPointerMove);
